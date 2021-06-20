7557acb66f8cbdcdaada52207bc2c3fa339a5054
V Roshan Kumar Patro <varanasiroshan31011@gmail.com>
2021-06-20 15:55:17 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -51,7 +51,7 @@ goto fail
 if not "%OS%" == "Windows_NT" goto win9xME_args
 
 :win9xME_args
-@rem Slurp the command line arguments.
+@rem Slurp the command line arguments
 set CMD_LINE_ARGS=
 set _SKIP=2
 
